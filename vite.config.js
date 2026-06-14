e1fd57e30a97df959b886a7f62da73620c6aa71e
Walter Carta <walter@Walters-MacBook-Air.local>
2026-06-14 09:08:12 +0100

    fix api key

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [],
-      }
-    })
-  ],
-  esbuild: false,
+  plugins: [react()],
 })
