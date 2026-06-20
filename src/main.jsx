0a58c2a2d8d0347c1d7812c65994caf03a99a79d
Walter Carta <walter@Walters-MacBook-Air.local>
2026-06-21 00:18:18 +0100

    fix cases query, remove debug log

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,6 @@ function Root() {
         .eq('user_id', u.id)
         .maybeSingle()
 
-      console.log("memberData:", memberData);
       if(memberData) {
         setOrg(memberData.organisations)
         setMember({ role: memberData.role, name: memberData.name })
